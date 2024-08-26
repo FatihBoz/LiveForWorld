b7d3112f74c26f31d0851e21178b475e0a9a516e
Mustafa Cetin <115427747+mustafa-cetin@users.noreply.github.com>
2024-08-26 19:29:22 +0300

    attack controller seperated

---
+++ Assets/Scripts/Player/PlayerAttackController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerAttackController : MonoBehaviour
+{
+
+
+    public float BulletSpeed = 5f;
+    public GameObject BulletPrefab;
+    public Transform ShootPoint;
+
+    private Animator animator;
+
+    private LayerMask layerMask;
+
+    private int score=0;
+    void Start()
+    {
+        layerMask= LayerMask.GetMask("Enemy");
+        animator = GetComponent<Animator>();
+        InputManager.Instance.input.Player.Shoot.performed += Shoot;
+    }
+      private void Shoot(InputAction.CallbackContext context)
+    {
+        animator.SetTrigger("Shoot");
+        GameObject bullet = Instantiate(BulletPrefab, ShootPoint.position, transform.rotation);
+        bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.rotation.x * BulletSpeed,0, bullet.transform.rotation.z * BulletSpeed);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position,transform.forward,out hit,Mathf.Infinity,layerMask))
+        {
+            Destroy(hit.collider.gameObject);
+            score+=10;
+            Debug.Log(score);
+        }
+
+
+    }
+
+
+}
