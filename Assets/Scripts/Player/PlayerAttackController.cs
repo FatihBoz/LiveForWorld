e592c04ee57099e1375d5fbab215374ea4daf683
Fatih <faatihboz@gmail.com>
2024-08-27 22:30:11 +0300

    Little Updates

--- Assets/Scripts/Player/PlayerAttackController.cs
+++ Assets/Scripts/Player/PlayerAttackController.cs
@@ -38,6 +38,8 @@ public class PlayerAttackController : MonoBehaviour
         StartCoroutine(StartShootingRoutine());
     }
 
+
+
     private IEnumerator StartShootingRoutine()
     {
         while (isShooting)
