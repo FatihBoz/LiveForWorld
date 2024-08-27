78dc4a33f5103bf13d6c9004ee49e66d82190e09
Niyazi Emre Akbaş <emreakbas46@outlook.com>
2024-08-27 18:55:20 +0300

    Level backup

---
+++ Assets/Landmine.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Landmine : Building
+{
+    public override void UpgradeBuilding()
+    {
+        throw new System.NotImplementedException();
+    }
+}
