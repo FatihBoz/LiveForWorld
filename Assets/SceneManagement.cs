de54078d5ec16504d5fac61e8a8c19429045449a
Fatih <faatihboz@gmail.com>
2024-08-28 16:16:16 +0300

    Mustafaya yazdır

--- Assets/SceneManagement.cs
+++ Assets/SceneManagement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,31 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagement : MonoBehaviour
 {
+    public GameObject PlayerDeadScreen;
     private void ChangeScene()
     {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    private void OnEnable()
+    {
+        PlayerProperties.OnPlayerDeath += Scene_OnPlayerDeath;
+    }
+
+    private void Scene_OnPlayerDeath()
+    {
+        PlayerDeadScreen.SetActive(true);
+        StartCoroutine(Delay());
+    }
+
+    private void OnDisable()
+    {
+        PlayerProperties.OnPlayerDeath -= Scene_OnPlayerDeath;
+    }
+
+    private IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(2f);
+        ChangeScene();
     }
 }
